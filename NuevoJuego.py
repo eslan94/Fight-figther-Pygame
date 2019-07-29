0dc72695ed1091c94f7637ad2db731be06b5cf3c
JonathanIsraelVasquez <jon2013.p@hotmail.com>
2019-07-28 20:11:52 -0500

    Implementacion de la clase del segundo personaje y las colisiones por parte de cada uno

--- NuevoJuego.py
+++ NuevoJuego.py
@@ -63,3 +63,63 @@ class Personaje(Sprite):
             if self.rect.y < 530:
                 self.image = personaje = pygame.image.load("Personajes/gokudown.png").convert_alpha()
                 self.rect.y += 10
+
+class Kamehameha(Sprite):
+    def __init__(self):
+        self.image = kamehameha = pygame.image.load("Personajes/kamehameha.gif").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.move_ip(900, 700)
+    def update(self):
+        teclas = pygame.key.get_pressed()
+        if self.rect.x > 840:
+            if teclas[K_SPACE]:
+                self.rect.x = (personaje.rect.x + 60)
+                self.rect.y = (personaje.rect.y + 14)
+        if self.rect.x < 870:
+            self.rect.x += 20
+
+class Minicell(Sprite):
+    def __init__(self):
+        self.image = minicell = pygame.image.load("Personajes/minicell.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.move_ip(750, 300)
+        self.bandera = 0
+        self.muerto = 0
+    def update(self):
+        teclas = pygame.key.get_pressed()
+        if teclas[K_e]:
+            self.image = personaje = pygame.image.load("Personajes/disparominicell.png").convert_alpha()
+        elif kamehameha.rect.x > 860:
+            self.image = personaje = pygame.image.load("Personajes/minicell.png").convert_alpha()
+
+        if teclas[K_a]:
+            self.image = personaje = pygame.image.load("Personajes/minicell.png").convert_alpha()
+            if self.rect.x > 0:
+                self.rect.x -= 10
+        elif teclas[K_d]:
+            self.image = personaje = pygame.image.load("Personajes/minicell.png").convert_alpha()
+            if self.rect.x < 740:
+                self.rect.x += 10
+
+        if teclas[K_w]:
+            self.image = personaje = pygame.image.load("Personajes/minicell.png").convert_alpha()
+            if self.rect.y > 32:
+                self.rect.y -= 10
+        elif teclas[K_s]:
+            if self.rect.y < 530:
+                self.image = personaje = pygame.image.load("Personajes/minicell.png").convert_alpha()
+                self.rect.y += 10
+
+class Disparo(Sprite):
+    def __init__(self):
+        self.image = barravidagoku = pygame.image.load("Personajes/disparominicell.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.move_ip(-400, -400)
+    def update(self):
+        teclas = pygame.key.get_pressed()
+        if self.rect.x == -400:
+            if teclas[K_e]:
+                self.rect.x = (minicell.rect.x - 60)
+                self.rect.y = (minicell.rect.y - 14)
+        if self.rect.x > -400:
+            self.rect.x -= 20
